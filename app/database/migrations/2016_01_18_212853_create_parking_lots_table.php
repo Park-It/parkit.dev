25b9d4b3da9e304435b8f560ebe57a3c97202509
jeromericks <jeromericks@gmail.com>
2016-01-18 15:43:51 -0600

    Added all of the migrations and seeders

---
+++ app/database/migrations/2016_01_18_212853_create_parking_lots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateParkingLotsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('parking_lots', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 255);
+			$table->string('address', 255);
+			$table->decimal('price', 8, 2);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('parking_lots');
+	}
+
+}
