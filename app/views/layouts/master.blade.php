7e00fa8c7ba4462d086e17f5f154752f873a3db9
jeromericks <jeromericks@gmail.com>
2016-01-18 18:00:13 -0600

    Added content to navbar and icon for site

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,6 +7,7 @@
     	<!-- Add CSRF Token as a meta tag in your head -->
     	<meta name="csrf-token" content="{{{ csrf_token() }}}">
     	@yield('title')
+        <link rel="shortcut icon" href="/img/parking.png">
     	{{-- Link tag for Bootstrap --}}
     	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
