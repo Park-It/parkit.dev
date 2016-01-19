7b5badf568d9eae34a2d0d61b61ce6a967284880
jeromericks <jeromericks@gmail.com>
2016-01-18 18:27:08 -0600

    Added google map to index page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,14 +5,15 @@
 @stop
 
 @section('top-script')
-
+	<link rel="stylesheet" type="text/css" href="/css/index.css">
 @stop
 
 @section('content')
-	
+	<div id="map-canvas"></div>
 @stop
 
 @section('bottom-script')
-
+	<script src="/js/map.js"></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb0ttOlTfxWFBvkLFiAh37EVNdwBA0xyM&signed_in=true&callback=initMap"></script>
 @stop
 
