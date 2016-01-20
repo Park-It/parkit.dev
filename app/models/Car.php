a58ddc1759743e639964e6c2bca2606742b20c1a
JacobGlatt <jglatt3@att.net>
2016-01-20 11:28:37 -0600

    rules for cars

--- app/models/Car.php
+++ app/models/Car.php
@@ -2,9 +2,16 @@
 
 class Car extends \Eloquent {
 
+	protected $table = "cars";
+	
 	// Add your validation rules here
 	public static $rules = [
 		// 'title' => 'required'
+		"make" => "required|max:255",
+		"model" => "required|max:255",
+		"license_number" => "required|max:10",
+		"color" => 'required|max:150'
+
 	];
 
 	// Don't forget to fill this array
