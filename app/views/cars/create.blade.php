e92654652e9101b3244efed073e92862c7330d68
jeromericks <jeromericks@gmail.com>
2016-01-20 12:25:02 -0600

    Added basic layout to the cars create and index pages

--- app/views/cars/create.blade.php
+++ app/views/cars/create.blade.php
@@ -5,5 +5,27 @@
 @stop
 
 @section('content')
-	
+	<div class="container">
+		<div class="rows">
+			<form>
+				<div class="form-group">
+					<label for="make">Make</label>
+					<input type="text" class="form-control" placeholder="Please enter your car's make">
+				</div>
+				<div class="form-group">
+					<label for="model">Model</label>
+					<input type="text" class="form-control" placeholder="Please enter your car's model">
+				</div>
+				<div class="form-group">
+					<label for="license_number">License Number</label>
+					<input type="text" class="form-control" placeholder="Please enter your car's license number">
+				</div>
+				<div class="form-group">
+					<label for="color">Color</label>
+					<input type="text" class="form-control" placeholder="Please enter your car's color">
+				</div>
+				<button class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	</div>
 @stop
--- app/views/cars/index.blade.php
+++ app/views/cars/index.blade.php
@@ -9,5 +9,24 @@
 		<div class="rows">
 			<a href="{{{ action('CarsController@create') }}}"><i class="fa fa-plus"></i>&nbsp;Add a Car</a>
 		</div>
+		<h2>Your Vehicles</h2>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Make</th>
+					<th>Model</th>
+					<th>License Number</th>
+					<th>Color</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>{{{ Auth::user()->car_id }}}</td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 @stop
