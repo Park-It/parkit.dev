f5ae73974829e183209f8a219d060cd118770595
jeromericks <jeromericks@gmail.com>
2016-01-22 09:11:38 -0600

    Added rules to the PreferredParkingLot model and fixed the layout of the users.edit page

--- app/models/PreferredParkingLot.php
+++ app/models/PreferredParkingLot.php
@@ -4,7 +4,10 @@ class PreferredParkingLot extends \Eloquent {
 
 	// Add your validation rules here
 	public static $rules = [
-		// 'title' => 'required'
+		'name' => 'required|max:255',
+		'address' => 'required|max:255',
+		'price' => 'required|max:255',
+		'max_spots' => 'required|max:255'
 	];
 
 	// Don't forget to fill this array
