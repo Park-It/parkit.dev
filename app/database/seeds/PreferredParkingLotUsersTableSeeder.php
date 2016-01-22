5ac6a06ef898b5360db39330a5c3f12787aac0f8
jeromericks <jeromericks@gmail.com>
2016-01-21 19:49:25 -0600

    Added more content into the PreferredParkingLotUsersTableSeeder

--- app/database/seeds/PreferredParkingLotUsersTableSeeder.php
+++ app/database/seeds/PreferredParkingLotUsersTableSeeder.php
@@ -11,10 +11,13 @@ class PreferredParkingLotUsersTableSeeder extends Seeder {
 
 		$user = User::firstOrFail();
 
-		$preferred_parking_lot_user = new PreferredParkingLotUser();
-		$preferred_parking_lot_user->preferred_parking_lot_id = $preferred_parking_lot->id;
-		$preferred_parking_lot_user->user_id = $user->id;
-		$preferred_parking_lot_user->save(); 
+		foreach(range(1, 10) as $index)
+		{
+			$preferred_parking_lot_user = new PreferredParkingLotUser();
+			$preferred_parking_lot_user->preferred_parking_lot_id = $preferred_parking_lot->id;
+			$preferred_parking_lot_user->user_id = $user->id;
+			$preferred_parking_lot_user->save(); 
+		}
 	}
 
 }
