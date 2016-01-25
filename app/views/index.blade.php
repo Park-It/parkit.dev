cfcbe876771c29adbfe5544e977c4544806f0f8c
jeromericks <jeromericks@gmail.com>
2016-01-25 15:10:34 -0600

    Find Me button now finds your location

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -45,5 +45,7 @@
 	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb0ttOlTfxWFBvkLFiAh37EVNdwBA0xyM&callback=initMap"></script>
 	<script src="/js/form-effect.js"></script>
 	<script src="/js/parking_lots.js"></script>
+	<script src="/js/find_me.js"></script>
+	<script src="/js/parking_lot_form.js"></script>
 @stop
 
