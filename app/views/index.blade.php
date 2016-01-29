f62398eabe5e66de84aa323888731aa8cf97f26a
jeromericks <jeromericks@gmail.com>
2016-01-28 21:12:49 -0600

    Fixed the styling of the footer to more closely match the styling of the navbar

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -87,22 +87,18 @@
             	<div class="add-Car"></div>
                 <div class="modal-body">
                     <div class="form-group">
-                        {{ $errors->first('make', '<span class="help-block alert alert-danger">:message</span>') }}
                         <label for="make">Make</label>
                         <input type="text" class="form-control" placeholder="Please enter your car's make" name="make" id="make">
                     </div>
                     <div class="form-group">
-                        {{ $errors->first('model', '<span class="help-block alert alert-danger">:message</span>') }}
                         <label for="model">Model</label>
                         <input type="text" class="form-control" placeholder="Please enter your car's model" name="model" id="model">
                     </div>
                     <div class="form-group">
-                        {{ $errors->first('license_plate_number', '<span class="help-block alert alert-danger">:message</span>') }}
                         <label for="license_plate_number">License Number</label>
                         <input type="text" class="form-control" placeholder="Please enter your car's license plate number" name="license_plate_number" id="license_plate_number">
                     </div>
                     <div class="form-group">
-                        {{ $errors->first('color', '<span class="help-block alert alert-danger">:message</span>') }}
                         <label for="color">Color</label>
                         <input type="text" class="form-control" placeholder="Please enter your car's color" name="color" id="color">
                     </div>
