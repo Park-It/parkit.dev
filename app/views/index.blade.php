3db009cc57825011f6a180af5e985508e20e1b88
Craig Re <car.code240z@gmail.com>
2016-01-29 09:10:31 -0600

    Recommiting changes again.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -9,7 +9,7 @@
 @stop
 
 @section('content')
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"> 
+        <div id="carousel-example-generic" class="carousel-slide" data-ride="carousel"> 
         <ol class="carousel-indicators"> 
             <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li> 
             <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
